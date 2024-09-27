2a0fda95ffef8ea1f35df6a7d52f1e2fc2804530
Anurag1195 <anuragyadav1195@gmail.com>
2024-09-27 12:21:31 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,5 +1,7 @@
 class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
+        #if the grid is empty , return 0 islands
+        if not grid or not
                     
         return 0
