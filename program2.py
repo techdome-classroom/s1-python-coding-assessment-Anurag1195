1b9086fccb3286a669467472c1042a008e37aa47
Anurag1195 <anuragyadav1195@gmail.com>
2024-09-27 13:01:19 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -11,9 +11,6 @@ def decode_message(s: str, p: str) -> bool:
             return memo[(i, j)]
         
         if j < len(p) and p[j] == '*':
-            # Try two possibilities:
-            # 1. `*` matches zero characters (move to the next key character)
-            # 2. `*` matches at least one character (move to the next message character)
             if match(i, j + 1) or (i < len(s) and match(i + 1, j)):
                 memo[(i, j)] = True
                 return True
