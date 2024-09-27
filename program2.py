de30e5a0602e3ef99ddb61add7882aaa14a8e0cb
Anurag1195 <anuragyadav1195@gmail.com>
2024-09-27 12:59:30 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,28 +1,5 @@
-def decode_message(self, message: str, key: str) -> bool:
-        memo = {}
-        
-        def match(i, j):
-            if i == len(message) and j == len(key):
-                return True
-           
-            if j == len(key):
-                return False
-            
-            if (i, j) in memo:
-                return memo[(i, j)]
-            
-            if j < len(key) and key[j] == '*':
-                if match(i, j + 1) or (i < len(message) and match(i + 1, j)):
-                    memo[(i, j)] = True
-                    return True
-            
-           
-            if i < len(message) and j < len(key) and (key[j] == '?' or key[j] == message[i]):
-                if match(i + 1, j + 1):
-                    memo[(i, j)] = True
-                    return True
-            
-            memo[(i, j)] = False
-            return False
-        
-        return match(0, 0)
+def decode_message( s: str, p: str) -> bool:
+
+# write your code here
+  
+        return False
