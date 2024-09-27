ad9ce3bb9e3f37163e5c17cd54ed3a11f5a43516
Anurag1195 <anuragyadav1195@gmail.com>
2024-09-27 12:35:56 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -26,5 +26,5 @@ class Solution:
 
         for r in range (rows):
             for c in range (cols):
-                if(grid[r][c]             
+                if grid[r][c] == 'L' and not visited[r]             
         return 0
